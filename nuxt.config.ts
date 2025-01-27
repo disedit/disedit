fe0c9a5c5912fe30921fe87d9698f738ae9731a4
Guillem <guillem@disedit.com>
2025-01-27 19:17:50 +0100

    add google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,18 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [
       tailwindcss(),
     ],
   },
+
+  modules: ['@nuxtjs/google-fonts'],
+
+  googleFonts: {
+    families: {
+      'IBM+Plex+Sans': [500]
+    }
+  }
 })
