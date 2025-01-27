766188bd60c731af326c94d77a17fbf13329b8c8
Guillem <guillem@disedit.com>
2025-01-27 19:01:41 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,45 @@
+<script setup>
+useHead({
+  bodyAttrs: {
+    class: 'bg-black'
+  },
+  link: [
+    { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png', sizes: '96x96' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'shortcut icon',href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+  ]
+})
+
+const title = 'disEdit - Web development for organizations'
+const description = 'We develop custom-made digital tools and websites for organizations and non-profits'
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+  ogImage: 'https://disedit.com/ogimage.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
+<template>
+  <div class="flex flex-col h-[100vh] p-site text-white items-start text-md">
+    <Logo class="w-full md:w-auto md:h-[9rem]" />
+
+    <p class="mt-auto text-xl text-balance leading-[1.1]">
+      We develop <span class="text-blue">custom-made digital tools
+        <br class="hidden md:block">
+        and websites</span> for organizations and non-profits.
+    </p>
+
+    <p class="mt-6">
+      Get in touch with us at
+      <a href="mailto:info@disedit.com" class="text-orange hover:underline">
+        info@disedit.com
+      </a>
+    </p>
+  </div>
+</template>
